16e6248b9d81699a4e7af524af7e507634fdf431
Daniel Burla <dburla2@gmail.com>
2022-02-28 15:59:10 -0500

    labs Fundamentals

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,28 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        byte b = 127;
+        System.out.println("byte b is: " + b);
+
+        short s = 25000;
+        System.out.println("short s is: " + s);
+
+        long l = 2540225456l;
+        System.out.println("long l is: " + l);
+
+        boolean bool = true;
+        System.out.println("boolean bool is: " + bool);
+
+        char c = 'A';
+        System.out.println("char c is: " + c);
+
+        float f = 3.36544155844566887456577866411223f;
+        System.out.println("float f is: " + f);
+
+        double d = 5.36544155844566887456577866411223;
+        System.out.println("double d is: " + d);
+
+
 
     }
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,27 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int size = str.length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean is_equal = str.equals(str2);
+        System.out.println(is_equal);
         // please concatenate str & str2 and set the result to a new String variable below
+        String concat = str + str2;
 
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+
+        System.out.println(concat.replace('e', '3'));
+        System.out.println(concat.substring(2, 5));
+        System.out.println(concat.contains("hello"));
+
+
+
+
+
+
 
     }
 
